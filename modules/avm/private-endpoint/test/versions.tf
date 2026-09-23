51ef0081c6159442f1bd91e1aef977faa6bbd299
Pratibha Shenoy <prshen@microsoft.com>
2026-09-23 15:45:49 +0530

    fix error

--- modules/avm/private-endpoint/test/versions.tf
+++ modules/avm/private-endpoint/test/versions.tf
@@ -14,6 +14,7 @@ terraform {
 provider "azurerm" {
   features {}
 
-  subscription_id = var.subscription_id
-  tenant_id       = var.tenant_id
+  subscription_id     = var.subscription_id
+  tenant_id           = var.tenant_id
+  storage_use_azuread = true
 }
